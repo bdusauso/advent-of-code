dd608ff5d64bcd951594585720d1452bb7ad7da9
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2016-12-03 11:50:10 +0100

    Working solution for d02/p01

--- 2016/02/keypad.ex
+++ 2016/02/keypad.ex
@@ -1,17 +1,35 @@
 defmodule AoC.Keypad do
 
-  @center_idx 4
+  @start_key_idx 4
 
   @vertical_move    3
   @horizontal_move  1
 
-  def unlock(input_lines) do
+  def get_secret_code(input_lines) do
+    last_key = input_lines
+    |> Enum.reduce(@start_key_idx,
+      fn input_line, next_start_key ->
+        IO.puts "=> Digit: #{next_start_key + 1}"
+        next_key(input_line, next_start_key)
+      end)
+    IO.puts "=> Digit: #{last_key + 1}"
   end
 
-  defp next_key(input_line, start_key), do:
+  def next_key(input_line, start_key) do
+    input_line
+    |> String.codepoints
+    |> Enum.reduce(start_key, fn movement, start_index -> move(movement, start_index) end)
+  end
 
-  defp move("U", key_index), do: key_index - vertical_move
-  defp move("D", key_index), do: key_index + vertical_move
-  defp move("L", key_index), do: key_index - horizontal_move
-  defp move("R", key_index), do: key_index + horizontal_move
+  defp move("U", key_index) when key_index <= 2, do: key_index
+  defp move("U", key_index), do: key_index - @vertical_move
+
+  defp move("D", key_index) when key_index >= 7, do: key_index
+  defp move("D", key_index), do: key_index + @vertical_move
+
+  defp move("L", key_index) when rem(key_index, 3) == 0, do: key_index
+  defp move("L", key_index), do: key_index - @horizontal_move
+
+  defp move("R", key_index) when rem(key_index, 3) == 2, do: key_index
+  defp move("R", key_index), do: key_index + @horizontal_move
 end
