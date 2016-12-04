9628fc8ee8e1cab2ba5a6b3d7b5d37bf0b34e15a
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2016-12-04 14:03:51 +0100

    First working solution for day 4

---
+++ 2016/04/room.ex
@@ -0,0 +1,39 @@
+defmodule AoC.Room do
+
+  def real(name) do
+    calculate_checksum(name) == extract_checksum(name)
+  end
+
+  def sector_id(name), do: ~r/([0-9]{1,})/ |> first_match(name)
+
+  def extract_checksum(name), do: ~r/\[([a-z]{1,})\]$/ |> first_match(name)
+
+  def calculate_checksum(name) do
+    name
+    |> String.split("-")
+    |> Enum.drop(-1)
+    |> Enum.join
+    |> letter_occurrences
+    |> Enum.to_list
+    |> Enum.sort_by(fn {_, count} -> count end, &>=/2)
+    |> Enum.take(5)
+    |> Enum.reduce("", fn e, x -> x <> elem(e, 0) end)
+  end
+
+  defp letter_occurrences(name) do
+    name
+    |> String.codepoints
+    |> Enum.reduce(%{},
+      fn letter, occurences ->
+        occurences
+        |> Map.update(letter, 1, &(&1+ 1))
+      end)
+  end
+
+  defp first_match(regex, str) do
+    regex
+    |> Regex.run(str)
+    |> Enum.take(-1)
+    |> Enum.fetch!(0)
+  end
+end
