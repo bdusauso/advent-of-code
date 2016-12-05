7f1a9d2b2176230bec829d7dc89eb70301de9d8b
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2016-12-05 21:09:41 +0100

    Working solution for part 1 of day 5

---
+++ 2016/05/md5.ex
@@ -0,0 +1,12 @@
+defmodule AoC.Password do
+
+  def guess(input) do
+    Stream.iterate(0, &(&1 + 1))
+    |> Stream.map(&(:erlang.md5("#{input}#{&1}")) |> Base.encode16(case: :lower))
+    |> Stream.filter(&(String.starts_with?(&1, "00000")))
+    |> Stream.map(&(String.at(&1, 5)))
+    |> Enum.take(8)
+    |> Enum.join
+  end
+
+end
