6a68cc6e127d2787f731f35eabe9ee0215695c01
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2016-12-06 07:52:10 +0100

    Working solution for day 6 part 1

---
+++ 2016/06/part1.ex
@@ -0,0 +1,28 @@
+defmodule AoC.NoiseSignal do
+
+  def remove_noise(signal) do
+    for i <- 0..7 do
+      signal
+      |> Enum.map(&(String.at(&1, i)))
+      |> count_letter_occurences
+      |> extract_highest_occurrences_letters
+    end |> Enum.join
+  end
+
+  defp count_letter_occurences(signal) do
+    signal
+    |> Enum.reduce(%{},
+      fn letter, occurences ->
+        occurences
+        |> Map.update(letter, 1, &(&1+ 1))
+      end)
+  end
+
+  defp extract_highest_occurrences_letters(occurences) do
+    occurences
+    |> Enum.to_list
+    |> Enum.max_by(fn {_, x} -> x end)
+    |> elem(0)
+  end
+
+end
