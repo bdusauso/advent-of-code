8984569404c89dffc3e84c05d201b2eaeaf2b938
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2016-12-08 14:13:03 +0100

    Working solution for part 1 of day 7

---
+++ 2016/07/part1.ex
@@ -0,0 +1,34 @@
+defmodule AoC.Abba do
+
+  def count_tls(input) do
+    input
+    |> Enum.filter(&support_tls/1)
+    |> length
+  end
+
+  def support_tls(ip_address) do
+    [outside, inside] = partition(ip_address)
+    outs = outside |> Enum.any?(&abba/1)
+    ins  = inside  |> Enum.any?(&abba/1)
+
+    outs && !ins
+  end
+
+  defp abba(ip_chunk) do
+    case ~r/(.)(.)(\2\1)/ |> Regex.run(ip_chunk) do
+      [_, a, b, _] -> a != b
+      _            -> false
+    end
+  end
+
+  defp partition(ip_address) do
+    ip_address
+    |> String.replace(~r/(\[|\])/, ",")
+    |> String.split(",")
+    |> Enum.with_index
+    |> Enum.partition(fn {_, idx} -> rem(idx, 2) == 0 end)
+    |> Tuple.to_list
+    |> Enum.map(&(Enum.map(&1, fn e -> elem(e, 0) end)))
+  end
+
+end
