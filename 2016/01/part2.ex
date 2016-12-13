47c90bf94eefe814e704eafd28f96e19376f4935
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2016-12-13 17:14:42 +0100

    Moving one block at a time. Still failing to solve the problem, it seems ...

--- 2016/01/part2.ex
+++ 2016/01/part2.ex
@@ -5,9 +5,8 @@ defmodule AoC.Path do
     |> String.split(", ")
     |> Enum.reduce([{0, 0, :north}],
         fn path, acc ->
-          [walk(hd(acc), path) | acc]
+          acc ++ walk(List.last(acc), path)
         end)
-    |> Enum.reverse
   end
 
   def duplicates(paths) do
@@ -17,16 +16,16 @@ defmodule AoC.Path do
     |> Enum.max_by(&(elem(&1, 1)))
   end
 
-  defp walk({x, y, :north}, "L" <> blocks), do: {x - String.to_integer(blocks), y, :west}
-  defp walk({x, y, :south}, "L" <> blocks), do: {x + String.to_integer(blocks), y, :east}
+  defp walk({x, y, :north}, "L" <> blocks), do: for b <- 1..String.to_integer(blocks), do:  {x - b, y, :west}
+  defp walk({x, y, :south}, "L" <> blocks), do: for b <- 1..String.to_integer(blocks), do:  {x + b, y, :east}
 
-  defp walk({x, y, :east},  "L" <> blocks), do: {x, y + String.to_integer(blocks), :north}
-  defp walk({x, y, :west},  "L" <> blocks), do: {x, y - String.to_integer(blocks), :south}
+  defp walk({x, y, :east},  "L" <> blocks), do: for b <- 1..String.to_integer(blocks), do:  {x, y + b, :north}
+  defp walk({x, y, :west},  "L" <> blocks), do: for b <- 1..String.to_integer(blocks), do:  {x, y - b, :south}
 
-  defp walk({x, y, :north}, "R" <> blocks), do: {x + String.to_integer(blocks), y, :east}
-  defp walk({x, y, :south}, "R" <> blocks), do: {x - String.to_integer(blocks), y, :west}
+  defp walk({x, y, :north}, "R" <> blocks), do: for b <- 1..String.to_integer(blocks), do:  {x + b, y, :east}
+  defp walk({x, y, :south}, "R" <> blocks), do: for b <- 1..String.to_integer(blocks), do:  {x - b, y, :west}
 
-  defp walk({x, y, :east},  "R" <> blocks), do: {x, y - String.to_integer(blocks), :south}
-  defp walk({x, y, :west},  "R" <> blocks), do: {x, y + String.to_integer(blocks), :north}
+  defp walk({x, y, :east},  "R" <> blocks), do: for b <- 1..String.to_integer(blocks), do:  {x, y - b, :south}
+  defp walk({x, y, :west},  "R" <> blocks), do: for b <- 1..String.to_integer(blocks), do:  {x, y + b, :north}
 
 end
