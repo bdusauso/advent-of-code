04080fe7218f2bdb6a0dc6fed4ef2048f861ef9f
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2017-12-02 19:03:07 +0100

    Add some tests

--- 2017/01/captcha.exs
+++ 2017/01/captcha.exs
@@ -12,17 +12,7 @@ end
 next_digit = fn _ -> 1 end
 halfway_around = fn digits -> div(length(digits), 2) end
 
-IO.puts Captcha.captcha([1, 1, 2, 2], next_digit)
-IO.puts Captcha.captcha([1, 1, 1, 1], next_digit)
-IO.puts Captcha.captcha([1, 2, 3, 4], next_digit)
-IO.puts Captcha.captcha([9, 1, 2, 1, 2, 1, 2, 9], next_digit)
-
-IO.puts Captcha.captcha([1, 2, 1, 2], halfway_around)
-IO.puts Captcha.captcha([1, 2, 2, 1], halfway_around)
-IO.puts Captcha.captcha([1, 2, 3, 4, 2, 5], halfway_around)
-IO.puts Captcha.captcha([1, 2, 3, 1, 2, 3], halfway_around)
-IO.puts Captcha.captcha([1, 2, 1, 3, 1, 4, 1, 5], halfway_around)
-
+# Input sanitization
 input = "input.txt"
         |> File.read!()
         |> String.trim
@@ -33,3 +23,28 @@ IO.inspect(input)
 
 IO.puts Captcha.captcha(input, next_digit)
 IO.puts Captcha.captcha(input, halfway_around)
+
+# Testing
+ExUnit.start()
+defmodule CaptachTest do
+  use ExUnit.Case, async: true
+
+  test "Captcha with next digit" do
+    next_digit = fn _ -> 1 end
+    
+    assert Captcha.captcha([1, 1, 2, 2], next_digit) == 3
+    assert Captcha.captcha([1, 1, 1, 1], next_digit) == 4
+    assert Captcha.captcha([1, 2, 3, 4], next_digit) == 0
+    assert Captcha.captcha([9, 1, 2, 1, 2, 1, 2, 9], next_digit) == 9
+  end
+
+  test "Captcha with halfway_around" do
+    halfway_around = fn digits -> div(length(digits), 2) end
+    
+    assert Captcha.captcha([1, 2, 1, 2], halfway_around) == 6
+    assert Captcha.captcha([1, 2, 2, 1], halfway_around) == 0
+    assert Captcha.captcha([1, 2, 3, 4, 2, 5], halfway_around) == 4
+    assert Captcha.captcha([1, 2, 3, 1, 2, 3], halfway_around) == 12
+    assert Captcha.captcha([1, 2, 1, 3, 1, 4, 1, 5], halfway_around) == 4
+  end
+end
