560fffe6952a183c60e226ebe1f509af97b6cce8
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2017-12-03 20:17:37 +0100

    2017 Day 3 in Ruby Part 1

---
+++ 2017/03/manhattan.rb
@@ -0,0 +1,36 @@
+# Calculate the coordinates of a number on the grid
+# beginning at 1 = (0, 0) and going to the right.
+# Examples:
+# coordinates(1) = (0, 0)
+# coordinates(2) = (1, 0)
+# coordinates(7) = (-1, -1)
+# coordinates(16) = (-1, 2)
+# ...
+def max_number_on_line(line)
+  (2 * line + 1) ** 2
+end
+
+def coordinates(number)
+  corners       = [Complex(1, -1), Complex(1, 1), Complex(-1, 1), Complex(-1, -1)]
+  displacements = [Complex(0, 1), Complex(-1, 0), Complex(0, -1), Complex(1, 0)]
+
+  line                    = ((Math.sqrt(number) - 1) / 2).ceil()
+  line_length             = 2 * line + 1
+  max_number_on_line      = line_length ** 2
+  numbers_on_side         = (max_number_on_line(line) - max_number_on_line(line - 1)) / 4 
+  distance_to_number      = max_number_on_line - number
+  quotient                = distance_to_number / numbers_on_side 
+  remainder               = distance_to_number % numbers_on_side
+
+  position = (corners[quotient] * line + displacements[quotient] * remainder)
+  
+  position.imag.abs + position.real.abs
+end
+
+# puts coordinates(1).inspect
+# puts coordinates(2).inspect
+# puts coordinates(7).inspect
+# puts coordinates(16).inspect
+# puts coordinates(27).inspect
+
+puts coordinates(277678)
