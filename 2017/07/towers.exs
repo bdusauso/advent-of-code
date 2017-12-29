1c45ff7ae30ae8b9fea775c40a233e84a41bbcc4
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2017-12-29 19:30:46 +0100

    2017 Day 7 part 1. Working solution

--- 2017/07/towers.exs
+++ 2017/07/towers.exs
@@ -4,9 +4,20 @@ defmodule AoC do
   def extract_info(line) do
     Regex.named_captures(@regex, line)
     |> Map.update!("weight", &String.to_integer/1)
-    |> Map.update!("parents", &(String.split(&1, ", ")))
+    |> Map.update!("parents", fn x -> if x == "", do: nil, else: String.split(x, ", ") end)
   end
 
+  def find_bottom_of_tower(towers) do
+    names   = towers
+              |> Enum.map(&(Map.get(&1, "name")))
+
+    parents = towers 
+              |> Enum.reduce([], fn towers, acc -> [Map.get(towers, "parents") | acc] end) 
+              |> List.flatten 
+              |> Enum.filter(&(&1 != nil)) 
+    
+    (names -- parents) |> List.first
+  end
 end
 
 ExUnit.start
@@ -31,4 +42,32 @@ defmodule AoCTest do
       "parents" => nil
     }    
   end
+
+  test "find bottom of tower" do
+    towers = [
+      %{"name" => "pbga", "parents" => nil},
+      %{"name" => "xhth", "parents" => nil},
+      %{"name" => "ebii", "parents" => nil},
+      %{"name" => "havc", "parents" => nil},
+      %{"name" => "ktlj", "parents" => nil},
+      %{"name" => "fwft", "parents" => ["ktlj", "cntj", "xhth"]},
+      %{"name" => "qoyq", "parents" => nil},
+      %{"name" => "padx", "parents" => ["pbga", "havc", "qoyq"]},
+      %{"name" => "tknk", "parents" => ["ugml", "padx", "fwft"]},
+      %{"name" => "jptl", "parents" => nil},
+      %{"name" => "ugml", "parents" => ["gyxo", "ebii", "jptl"]},
+      %{"name" => "gyxo", "parents" => nil},
+      %{"name" => "cntj", "parents" => nil}
+    ]
+
+    assert AoC.find_bottom_of_tower(towers) == "tknk"
+  end
 end
+
+input = "input.txt"
+        |> File.read! 
+        |> String.trim 
+        |> String.split("\n")
+        |> Enum.map(&AoC.extract_info/1)
+
+IO.puts AoC.find_bottom_of_tower(input)
