3aa1e719ea939fea93cf12c69d5311ef8dc46f73
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2018-12-01 19:58:56 +0100

    Improve code

--- 2018/01/frequency.exs
+++ 2018/01/frequency.exs
@@ -1,30 +1,27 @@
 defmodule Frequency do
   
   def apply_changes(frequencies) do
-    frequencies 
-    |> Enum.reduce(0, fn frequency, sum -> apply_change(sum, frequency) end)
+    frequencies |> Enum.sum
   end
 
   def find_repetition(frequencies) do
     frequencies
     |> Stream.cycle
     |> Enum.reduce_while([0], fn frequency, partial_sums ->
-        current_sum = partial_sums |> List.first |> apply_change(frequency) 
+        current_sum = List.first(partial_sums) + frequency 
         new_partial_sums = [current_sum | partial_sums]
         
         if Enum.member?(partial_sums, current_sum), do: {:halt, new_partial_sums}, else: {:cont, new_partial_sums}
     end)
     |> List.first
   end
-
-  defp apply_change(start, "-" <> frequency), do: start - (String.to_integer(frequency))  
-  defp apply_change(start, "+" <> frequency), do: start + (String.to_integer(frequency))  
 end
 
 frequencies = 
   "input.txt"
   |> File.read!
   |> String.split("\n")
+  |> Enum.map(&String.to_integer/1)
 
 IO.puts(Frequency.apply_changes(frequencies))
 IO.puts(Frequency.find_repetition(frequencies))
