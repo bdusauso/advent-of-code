acd48462fb5c2c6f5aca3902caf2cc78c4f27128
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2018-12-04 08:58:02 +0100

    2018 Day 3 part 2 in Elixir

--- 2018/03/grid_hash.exs
+++ 2018/03/grid_hash.exs
@@ -1,20 +1,37 @@
 defmodule Grid do
 
-  defstruct content: %{}, size: 0
-
-  def new(size), do: %Grid{content: %{}, size: size}
-
-  def overlapping_count(coordinates, grid_size) do
-    grid = Enum.reduce(coordinates, new(grid_size), &claim/2)
-    grid.content |> Map.values |> Enum.filter(&(&1 >= 2)) |> length
+  def intact_claim(inputs) do
+    grid = Enum.reduce(inputs, %{}, &(claim(&2, &1)))
+    
+    Enum.reduce_while(inputs, nil, fn [input_id | measurements], acc ->
+      coordinates = calculate_coordinates(measurements)
+      if Enum.all?(coordinates, fn coordinate -> Map.fetch!(grid, coordinate) == {input_id, 1} end) do
+        {:halt, input_id}
+      else
+        {:cont, nil}
+      end
+    end)
   end
 
-  def claim([id, left, top, width, height], %Grid{} = grid) do
-    IO.puts("Processing id ##{id}")
-    to_check = for i <- 0..(height - 1), j <- 0..(width - 1), do: {top + i, left + j}
-    Enum.reduce(to_check, grid, fn i, acc -> 
-      %Grid{acc | content: Map.update(acc.content, i, 1, &(&1 + 1))} 
-    end)
+  def overlapping_count(inputs) do
+    inputs
+    |> Enum.reduce(%{}, &(claim(&2, &1)))
+    |> Map.values 
+    |> Enum.filter(fn {_id, count} -> count >= 2 end) 
+    |> length
+  end
+
+  def claim(grid, [id | measurements]) do
+    # IO.puts("Processing id ##{id}")
+    measurements
+    |> calculate_coordinates
+    |> Enum.reduce(grid, fn i, acc -> 
+        Map.update(acc, i, {id, 1}, fn {id, count} -> {id, count + 1} end) 
+      end)
+  end
+
+  def calculate_coordinates([left, top, width, height]) do
+    for i <- 0..(height - 1), j <- 0..(width - 1), do: {top + i, left + j}
   end
 end
 
@@ -24,16 +41,25 @@ defmodule GridTest do
   use ExUnit.Case
 
   describe ".claim/1" do
-    grid = Grid.new(5)
-    grid = Grid.claim([0, 3, 2, 2, 2], grid)
-    
-    assert grid.content == %{{2, 3} => 1, {2, 4} => 1, {3, 3} => 1, {3, 4} => 1}
+    assert Grid.claim(%{}, [0, 3, 2, 2, 2]) == %{
+      {2, 3} => {0, 1}, 
+      {2, 4} => {0, 1}, 
+      {3, 3} => {0, 1}, 
+      {3, 4} => {0, 1}
+    }
   end
 end
 
-"input.txt"
-|> File.stream!([])
-|> Enum.map(&(Regex.scan(~r/-?\d+/, &1)))
-|> Enum.map(fn line -> line |>  List.flatten |> Enum.map(&String.to_integer/1) end)
-|> Grid.overlapping_count(1000)
-|> IO.inspect
+inputs =
+  "input.txt"
+  |> File.stream!([])
+  |> Enum.map(&(Regex.scan(~r/-?\d+/, &1)))
+  |> Enum.map(fn line -> line |>  List.flatten |> Enum.map(&String.to_integer/1) end)
+
+inputs
+  |> Grid.overlapping_count
+  |> IO.inspect(label: "Overlapping claims count")
+
+inputs
+  |> Grid.intact_claim
+  |> IO.inspect(label: "Only intact claim id")
