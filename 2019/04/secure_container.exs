c21ded7a7914b08085a9e92753276af9e47b2136
Bruno Dusausoy <bruno.dusausoy@ibanity.com>
2019-12-04 13:11:34 +0100

    2019/03/1/Elixir use built-in function 'Integer.digits/2' instead of creating my own

--- 2019/04/secure_container.exs
+++ 2019/04/secure_container.exs
@@ -4,7 +4,7 @@ defmodule Part1 do
   end
 
   def candidate?(number, from, to) when number in from..to do
-    number_list = number_to_list(number)
+    number_list = Integer.digits(number)
     one_double?(number_list) && increase?(number_list)
   end
   def candidate?(_, _, _), do: false
@@ -18,10 +18,6 @@ defmodule Part1 do
 
   def increase?([a, b, c, d, e, f]) when a <= b and b <= c and c <= d  and d <= e  and e <= f, do: true
   def increase?(_), do: false
-
-  def number_to_list(number), do: number_to_list(number, [])
-  def number_to_list(number, acc) when rem(number, 10) == 0, do: acc
-  def number_to_list(number, acc), do: number_to_list(div(number, 10), [rem(number, 10) | acc])
 end
 
 ExUnit.start()
@@ -33,17 +29,6 @@ defmodule Part1Test do
   describe "candidate?/3" do
     test "six-digits, within range, ascending order, one double" do
       assert candidate?(246678, 246515, 739105)
-    end
-  end
-
-  describe "number_to_list/1" do
-    test "one digit" do
-      assert number_to_list(2) == [2]
-      assert number_to_list(24) == [2, 4]
-      assert number_to_list(246) == [2, 4, 6]
-      assert number_to_list(2466) == [2, 4, 6, 6]
-      assert number_to_list(24667) == [2, 4, 6, 6, 7]
-      assert number_to_list(246678) == [2, 4, 6, 6, 7, 8]
     end
   end
 end
