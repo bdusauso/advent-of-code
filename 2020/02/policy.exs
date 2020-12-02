b6dc79f6e6612762d403bf2f9bdf9bb2feabff62
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2020-12-02 07:45:57 +0100

    2020 Day 2 Part 1 in Elixir

---
+++ 2020/02/policy.exs
@@ -0,0 +1,26 @@
+
+defmodule OldPolicy do
+  def valid?(%{"password" => password, "char" => char, "min" => min, "max" => max}) do
+    valid?(password, char, String.to_integer(min), String.to_integer(max))
+  end
+  def valid?(password, char, min, max) do
+    occurrences =
+      password
+      |> String.graphemes()
+      |> Enum.count(&(&1 == char))
+
+    occurrences in (min..max)
+  end
+end
+
+regex = ~r/(?<min>\d+)-(?<max>\d+) (?<char>[a-z]): (?<password>[a-z]+)/
+
+inputs =
+  "input.txt"
+  |> File.read!()
+  |> String.split("\n", trim: true)
+  |> Enum.map(&Regex.named_captures(regex, &1))
+
+inputs
+|> Enum.count(&OldPolicy.valid?/1)
+|> IO.inspect(label: "Part 1")
