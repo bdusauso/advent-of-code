fa5a7333837342c15fa1e28878889f339c205fb7
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2020-12-03 07:40:31 +0100

    Extract the algorithm into its own module

--- 2020/03/toboggan_trajectory.exs
+++ 2020/03/toboggan_trajectory.exs
@@ -3,26 +3,24 @@ input =
   |> File.read!()
   |> String.split("\n", trim: true)
 
-# Avoid computing it each time
-length =
-  input
-  |> List.first()
-  |> String.length()
+defmodule Traversal do
+  def tree_count(map, {right, down}) do
+    length =
+      map
+      |> List.first()
+      |> String.length()
 
-input
-|> Enum.with_index()
-|> Enum.count(fn {line, num} -> String.at(line, rem(num * 3, length)) == "#" end)
-|> IO.inspect(label: "Part 1")
-
-# Generalization of traversal.
-# Part 1 is a special case but could be computed using this function
-traversal = fn {right, down} ->
-  input
-  |> Enum.take_every(down)
-  |> Enum.with_index()
-  |> Enum.count(fn {line, num} -> String.at(line, rem(num * right, length)) == "#" end)
+    map
+    |> Enum.take_every(down)
+    |> Enum.with_index()
+    |> Enum.count(fn {line, num} -> String.at(line, rem(num * right, length)) == "#" end)
+  end
 end
 
+input
+|> Traversal.tree_count({3, 1})
+|> IO.inspect(label: "Part 1")
+
 [{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}]
-|> Enum.reduce(1, fn trees, product -> traversal.(trees) * product end)
+|> Enum.reduce(1, fn dir, product -> Traversal.tree_count(input, dir) * product end)
 |> IO.inspect(label: "Part 2")
