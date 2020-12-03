95eb8f9fceebfa76e6aae0487fbebc05d5a50699
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2020-12-03 07:29:38 +0100

    2020 Day 3 Part 2 in Elixir

--- 2020/03/toboggan_trajectory.exs
+++ 2020/03/toboggan_trajectory.exs
@@ -13,3 +13,16 @@ input
 |> Enum.with_index()
 |> Enum.count(fn {line, num} -> String.at(line, rem(num * 3, length)) == "#" end)
 |> IO.inspect(label: "Part 1")
+
+# Generalization of traversal.
+# Part 1 is a special case but could be computed using this function
+traversal = fn {right, down} ->
+  input
+  |> Enum.take_every(down)
+  |> Enum.with_index()
+  |> Enum.count(fn {line, num} -> String.at(line, rem(num * right, length)) == "#" end)
+end
+
+[{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}]
+|> Enum.reduce(1, fn trees, product -> traversal.(trees) * product end)
+|> IO.inspect(label: "Part 2")
