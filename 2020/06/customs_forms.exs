7d71c3a772bf167bfff1842c1fdf9eaa503ebde9
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2020-12-06 14:42:55 +0100

    2020 Day 6 Part 2 in Elixir

--- 2020/06/customs_forms.exs
+++ 2020/06/customs_forms.exs
@@ -8,12 +8,20 @@ defmodule CustomForms do
     |> Enum.reject(&(List.first(&1) == ""))
   end
 
-  @spec unanimity(list(String.t())) :: non_neg_integer()
-  def unanimity(group) do
+  @spec anyone(list(String.t())) :: non_neg_integer()
+  def anyone(group) do
     group
     |> Enum.flat_map(&String.graphemes/1)
     |> Enum.uniq()
     |> length()
+  end
+
+  @spec everyone(list(String.t())) :: non_neg_integer()
+  def everyone(group) do
+    group
+    |> Enum.flat_map(&String.graphemes/1)
+    |> Enum.frequencies()
+    |> Enum.count(fn {_, count} -> count == length(group) end)
   end
 end
 
@@ -22,17 +30,31 @@ ExUnit.start()
 defmodule CustomFormsTest do
   use ExUnit.Case
 
-  test "unanimity/1" do
+  test "anyone/1" do
     groups = [~w(abc), ~w(a b c), ~w(ab ac), ~w(a a a a), ~w(b)]
 
-    assert Enum.map(groups, &CustomForms.unanimity/1) == [3, 3, 3, 1, 1]
+    assert Enum.map(groups, &CustomForms.anyone/1) == [3, 3, 3, 1, 1]
+  end
+
+  test "everyone/1" do
+    groups = [~w(abc), ~w(a b c), ~w(ab ac), ~w(a a a a), ~w(b)]
+
+    assert Enum.map(groups, &CustomForms.everyone/1) == [3, 0, 1, 1, 1]
   end
 end
 
-"input.txt"
-|> File.read!()
-|> String.split("\n")
-|> CustomForms.groups()
-|> Enum.map(&CustomForms.unanimity/1)
+groups =
+  "input.txt"
+  |> File.read!()
+  |> String.split("\n")
+  |> CustomForms.groups()
+
+groups
+|> Enum.map(&CustomForms.anyone/1)
 |> Enum.sum()
 |> IO.inspect(label: "Part 1")
+
+groups
+|> Enum.map(&CustomForms.everyone/1)
+|> Enum.sum()
+|> IO.inspect(label: "Part 2")
