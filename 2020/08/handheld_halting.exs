447ed9f3033b48c0760445f76144a5b011d0de82
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2020-12-08 08:29:37 +0100

    2020 Day 8 Part 2 in Elixir. Also improved overall code

--- 2020/08/handheld_halting.exs
+++ 2020/08/handheld_halting.exs
@@ -1,23 +1,52 @@
 defmodule Handheld do
   def disassemble(<<instr::bytes-size(3)>> <> " " <> arg), do: {instr, String.to_integer(arg)}
 
+  def find_termination(operations) do
+    try do
+      for idx <- 0..length(operations) - 1 do
+        case operations |> Handheld.swap_instruction(idx) |> Handheld.detect_loop() do
+          {:termination, sum} -> throw({:ok, sum})
+          res -> res
+        end
+      end
+    catch
+      {:ok, sum} -> sum
+    end
+  end
+
+  def swap_instruction(operations, idx) do
+    new_operation =
+      case Enum.at(operations, idx) do
+        {"nop", amount} -> {"jmp", amount}
+        {"jmp", amount} -> {"nop", amount}
+        operation -> operation
+      end
+
+    List.replace_at(operations, idx, new_operation)
+  end
+
   def detect_loop(operations), do: detect_loop(operations, 0, 0, [])
 
   defp detect_loop(operations, cur_idx, sum, checked) do
-    if cur_idx in checked do
-      sum
-    else
-      checked = [cur_idx | checked]
-      case Enum.at(operations, cur_idx) do
-        {"acc", amount} ->
-          detect_loop(operations, cur_idx + 1, sum + amount, checked)
+    cond do
+      cur_idx in checked ->
+        {:loop, sum}
 
-        {"nop", _} ->
-          detect_loop(operations, cur_idx + 1, sum, checked)
+      cur_idx not in 0..length(operations) - 1 ->
+        {:termination, sum}
 
-        {"jmp", amount} ->
-          detect_loop(operations, cur_idx + amount, sum, checked)
-      end
+      true ->
+        checked = [cur_idx | checked]
+        case Enum.at(operations, cur_idx) do
+          {"acc", amount} ->
+            detect_loop(operations, cur_idx + 1, sum + amount, checked)
+
+          {"nop", _} ->
+            detect_loop(operations, cur_idx + 1, sum, checked)
+
+          {"jmp", amount} ->
+            detect_loop(operations, cur_idx + amount, sum, checked)
+        end
     end
   end
 end
@@ -30,4 +59,9 @@ operations =
 
 operations
 |> Handheld.detect_loop()
+|> elem(1)
 |> IO.inspect(label: "Part 1")
+
+operations
+|> Handheld.find_termination()
+|> IO.inspect(label: "Part 2")
