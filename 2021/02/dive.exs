b15f83e812261f665f1795fc8b595fd6ff5b5be2
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2021-12-02 18:06:57 +0100

    2021 Day 2 Part 2 in Elixir

--- 2021/02/dive.exs
+++ 2021/02/dive.exs
@@ -4,6 +4,12 @@ defmodule DivePart1 do
   def move({h, v}, ["up", x]), do: {h, v - x}
 end
 
+defmodule DivePart2 do
+  def move({h, v, a}, ["forward", x]), do: {h + x, v + (a * x), a}
+  def move({h, v, a}, ["down", x]), do: {h, v, a + x}
+  def move({h, v, a}, ["up", x]), do: {h, v, a - x}
+end
+
 moves =
   "input.txt"
   |> File.read!()
@@ -13,3 +19,6 @@ moves =
 
 {h, v} = Enum.reduce(moves, {0, 0}, fn move, pos -> DivePart1.move(pos, move) end)
 IO.inspect(h * v, label: "Part1")
+
+{h, v, _a} = Enum.reduce(moves, {0, 0, 0}, fn move, pos -> DivePart2.move(pos, move) end)
+IO.inspect(h * v, label: "Part2")
